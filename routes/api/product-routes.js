c02a97341787e605f32909bf5e772cea9b3fe660
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-15 00:05:24 -0400

    Got the product-routes.js routes working.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// find all products
+// be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productContent = await Product.findAll({
+      include: [{ model: Category },
+      { model: Tag }
+      ]
+    });
+    res.status(200).json(productContent);
+  } catch (error) {
+    res.status(500).json(error);
+  };
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// find a single product by its `id`
+// be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productContent = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
+    if (!productContent) {
+      return res.status(404).json({ message: 'Product cannot be found!' });
+    }
+    res.status(200).json(productContent);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error!' });
+  }
 });
 
 // create new product
@@ -89,8 +111,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productContent = await Product.destroy(
+      {
+        where: {
+          id: req.params.id,
+        }
+      });
+    res.json(productContent);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 module.exports = router;
