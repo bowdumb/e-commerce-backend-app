adb421ebba963c9f0d31ac32b16af20e0d7e3899
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-14 23:18:06 -0400

    Got the category-routes.js routes working.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,46 +1,79 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { afterSync } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
+// find all categories
+// be sure to include its associated Products
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   try {
     const categoryContent = await Category.findAll({
-      include: [{model: Product}]
+      include: [{ model: Product }]
     });
     res.status(200).json(categoryContent);
   } catch (error) {
-    res.status(400).json(error);
-  }
-  
+    res.status(500).json(error);
+  };
 });
+// find one category by its `id` value
+// be sure to include its associated Products
 
 router.get('/:id', async (req, res) => {
   try {
     const categoryContent = await Category.findOne({
-      where: {id:req.params.id},
-      include:[{model:Product}]
+      where: { id: req.params.id },
+      include: [{ model: Product }]
     });
     if (!categoryContent) {
-      return res.status()
+      return res.status(404).json({ message: 'Category cannot be found!' })
     }
+    res.status(200).json(categoryContent);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error!' })
+  };
+});
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryContent = await Category.create(req.body);
+    res.status(200).json(categoryContent);
+  } catch (error) {
+    res.status(500).json(categoryContent);
+  };
+});
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryContent = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.json(categoryContent);
+  } catch (error) {
+    res.json(error);
   }
-  // find one category by its `id` value
-  // be sure to include its associated Products
 });
-
-router.post('/', (req, res) => {
-  // create a new category
-});
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+ // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try{
+    const categoryContent = await Category.destroy(
+      {
+        where: {
+          id: req.params.id,
+        }
+      });
+    res.json(categoryContent);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 module.exports = router;
