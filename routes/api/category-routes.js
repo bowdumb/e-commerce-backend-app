be22c562c7590d0a098b7d43e2b19ace23d2a25b
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-14 21:29:02 -0400

    Got the server connected to the database and running.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,30 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryContent = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryContent);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+  
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryContent = await Category.findOne({
+      where: {id:req.params.id},
+      include:[{model:Product}]
+    });
+    if (!categoryContent) {
+      return res.status()
+    }
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
